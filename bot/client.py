ce04f99ba0c0aa334793ce3039cef0e2713c0eb6
Mahalakshmi K <1476mahalakshmi@gmail.com>
2026-05-01 21:48:36 +0530

    Update client.py

--- bot/client.py
+++ bot/client.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 def get_client():
     return Client(
-        os.getenv("BINANCE_API_KEY"),
-        os.getenv("BINANCE_SECRET_KEY"),
+        os.getenv("UiaGZNhZfv03bxGnjVPjWQZy41nGjjU21ZFodEcdkVeIH2RxKuFajZEnOY3eNdcE"),
+        os.getenv("hkqcF71W4Tgy58CwXd6j5Pt20KxC7xk1eXXdhDcJ86zWyMSwrxxXKFmF5myllh1X"),
         testnet=True
     )
